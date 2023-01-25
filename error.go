5606b228bd3e25d9ea3256f43bed6d25120968d1
Nigel Page <nigelp@synteca.com>
2023-01-26 08:22:43 +1100

    Support all OpenTelemetry severity ranges

--- error.go
+++ error.go
@@ -20,29 +20,48 @@ import (
 type Severity int16
 
 const (
-	TRACE = 1
-	DEBUG = 5
-	INFO  = 9
-	WARN  = 13
-	ERROR = 17
-	FATAL = 21
+	NOTSPECIFIED Severity = iota
+	TRACE
+	TRACE2
+	TRACE3
+	TRACE4
+	DEBUG
+	DEBUG2
+	DEBUG3
+	DEBUG4
+	INFO
+	INFO2
+	INFO3
+	INFO4
+	WARN
+	WARN2
+	WARN3
+	WARN4
+	ERROR
+	ERROR2
+	ERROR3
+	ERROR4
+	FATAL
+	FATAL2
+	FATAL3
+	FATAL4
 )
 
 type StructuredError struct {
-	Severity    Severity               // error severity
-	Code        string                 // error code
-	Message     string                 // error message
-	When        int64                  // time error occured (nanoseconds since Unix epoch - OpenTelemetry)
-	Attributes  map[string]interface{} // any additional information
+	Severity   Severity               // error severity
+	Code       string                 // error code
+	Message    string                 // error message
+	When       int64                  // time error occured (nanoseconds since Unix epoch - OpenTelemetry)
+	Attributes map[string]interface{} // any additional information
 }
 
 func NewStructuredError(severity Severity, code, msg string, attributes map[string]interface{}) *StructuredError {
 	return &StructuredError{
-		Severity:    severity,
-		Code:        strings.ToUpper(code),
-		Message: 	 msg,
-		When:        time.Now().Unix(),
-		Attributes:  attributes,
+		Severity:   severity,
+		Code:       strings.ToUpper(code),
+		Message:    msg,
+		When:       time.Now().Unix(),
+		Attributes: attributes,
 	}
 }
 
@@ -72,18 +91,56 @@ func (se *StructuredError) Error() string {
 func (se *StructuredError) severityText() string {
 	var sev string // error severity
 	switch se.Severity {
+	case NOTSPECIFIED:
+		sev = "NOTSPECIFIED"
 	case TRACE:
 		sev = "TRACE"
+	case TRACE2:
+		sev = "TRACE2"
+	case TRACE3:
+		sev = "TRACE3"
+	case TRACE4:
+		sev = "TRACE4"
 	case DEBUG:
 		sev = "DEBUG"
+	case DEBUG2:
+		sev = "DEBUG2"
+	case DEBUG3:
+		sev = "DEBUG3"
+	case DEBUG4:
+		sev = "DEBUG4"
 	case INFO:
 		sev = "INFO"
+	case INFO2:
+		sev = "INFO2"
+	case INFO3:
+		sev = "INFO3"
+	case INFO4:
+		sev = "INFO4"
 	case WARN:
 		sev = "WARN"
+	case WARN2:
+		sev = "WARN2"
+	case WARN3:
+		sev = "WARN3"
+	case WARN4:
+		sev = "WARN4"
 	case ERROR:
 		sev = "ERROR"
+	case ERROR2:
+		sev = "ERROR2"
+	case ERROR3:
+		sev = "ERROR3"
+	case ERROR4:
+		sev = "ERROR4"
 	case FATAL:
 		sev = "FATAL"
+	case FATAL2:
+		sev = "FATAL2"
+	case FATAL3:
+		sev = "FATAL3"
+	case FATAL4:
+		sev = "FATAL4"
 	}
 
 	return sev
