a75cda3fe83e192dfe78d9a7dca1c53230b3a6f1
Nigel Page <nigelp@synteca.com>
2023-02-02 07:39:57 +1100

    Update to Go 1.20

--- error_test.go
+++ error_test.go
@@ -19,7 +19,7 @@ var a = map[string]interface{}{
 }
 
 func TestNewStructuredError(t *testing.T) {
-	e, err := NewStructuredError(s, c, m, a)
+	e, err := NewStructuredError(s, c, m, a, nil)
 	if err != nil {
 		t.Errorf("NewStructuredError did not validate severity correctly")
 	}
@@ -35,7 +35,7 @@ func TestNewStructuredError(t *testing.T) {
 }
 
 func TestStructuredErrorIsErrorCode(t *testing.T) {
-	e, _ := NewStructuredError(s, c, m, a)
+	e, _ := NewStructuredError(s, c, m, a, nil)
 
 	if e.Code != strings.ToUpper(c) {
 		t.Errorf("StructuredError.IsErrorCode did not enforce uppercase error code")
@@ -43,7 +43,7 @@ func TestStructuredErrorIsErrorCode(t *testing.T) {
 }
 
 func TestError(t *testing.T) {
-	e, _ := NewStructuredError(s, c, m, a)
+	e, _ := NewStructuredError(s, c, m, a, nil)
 	var m = e.Error()
 	if m == "" {
 		t.Errorf("StructuredError.Error() did not return a formatted string")
@@ -51,7 +51,7 @@ func TestError(t *testing.T) {
 }
 
 func TestJson(t *testing.T) {
-	se, _ := NewStructuredError(s, c, m, a)
+	se, _ := NewStructuredError(s, c, m, a, nil)
 	j, e := se.ToJson()
 	if e != nil {
 		t.Errorf("StructuredErrorToJson failed to serialize StructuredError to Json")
